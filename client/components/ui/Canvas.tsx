3fa6351dac5e4eb86e4d199a96be3ec1eb0bf900
Unknowkubbrother <natchnon654321@gmail.com>
2025-03-06 01:37:14 +0700

    test

--- client/components/ui/Canvas.tsx
+++ client/components/ui/Canvas.tsx
@@ -37,7 +37,6 @@ export default function Canvas({ setData, width, height, className }: CanvasProp
         }
 
 
-        saveImageData();
     }, [history]);
 
 
@@ -63,40 +62,6 @@ export default function Canvas({ setData, width, height, className }: CanvasProp
         canvasDraw.undo();
     }
 
-    const saveImageData = () => {
-        const canvasDraw = canvasRef.current;
-        if (!canvasDraw) {
-            console.error("Canvas reference not found");
-            return;
-        }
-
-        // @ts-ignore: canvasRef.current.canvas is not typed, but "drawing" is the default canvas key
-        const canvasElement = canvasDraw.canvas["drawing"] || canvasDraw.canvas;
-
-        if (!canvasElement || !(canvasElement instanceof HTMLCanvasElement)) {
-            console.error("Could not access the canvas element");
-            return;
-        }
-
-        const tempCanvas = document.createElement("canvas");
-        tempCanvas.width = 224;
-        tempCanvas.height = 224;
-        const tempCtx = tempCanvas.getContext("2d");
-        if (!tempCtx) return;
-        tempCtx.fillStyle = "white";
-        tempCtx.fillRect(0, 0, 224, 224);
-        tempCtx.drawImage(canvasElement, 0, 0, 224, 224);
-
-        const dataURL = tempCanvas.toDataURL("image/jpeg");
-
-        if (!dataURL) {
-            console.error("Could not save the image");
-            return;
-        }
-
-        setData(dataURL);
-
-    };
 
     return (
         <div className={cn("flex flex-col gap-5", { className })}>
